a5ade067f886731fcac952e3395ae4795b2fd3c3
Shlomo Hassid <shlomohassid@gmail.com>
2025-05-17 22:50:46 +0300

    feat: add inputDir handling and hot update support in i18nJsonPlugin

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.i18nJsonPlugin = i18nJsonPlugin;
 const utils_1 = require("./utils");
+const path_1 = __importDefault(require("path"));
 function i18nJsonPlugin(options = {}) {
     const inputPath = options.inputPath || 'src/i18n';
     const outputDir = options.outputDir || 'public/locales';
+    const inputDir = inputPath.endsWith('.json') || inputPath.endsWith('.jsonc') ? path_1.default.dirname(inputPath) : inputPath;
     let singleFile = false;
     // if its a file wrap it in an array:
     if (inputPath.endsWith('.jsonc') || inputPath.endsWith('.json')) {
@@ -23,6 +28,28 @@ function i18nJsonPlugin(options = {}) {
             else {
                 await (0, utils_1.processI18nFiles)(inputPath, outputDir);
             }
+        },
+        async handleHotUpdate(ctx) {
+            const changedDir = path_1.default.dirname(ctx.file);
+            const changedFile = ctx.file;
+            // is inputDir in the file path:
+            if (singleFile && changedFile.endsWith(inputPath) || !singleFile && changedDir.endsWith(inputDir)) {
+                if (singleFile) {
+                    await (0, utils_1.processI18nFile)(inputPath, outputDir);
+                }
+                else {
+                    await (0, utils_1.processI18nFiles)(inputPath, outputDir);
+                }
+            }
+            else {
+                return;
+            }
+        },
+        configureServer(server) {
+            // the path of outputDir use the config from vite.config.ts
+            const theAbsPath = path_1.default.resolve(server.config.root, outputDir);
+            // Replace with your actual output directory
+            server.watcher.unwatch(`${theAbsPath}/**`);
         }
     };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Plugin } from 'vite';
 import { processI18nFile, processI18nFiles } from './utils';
+import path from 'path';
 
 export interface I18nPluginOptions {
   inputPath?: string;
@@ -9,6 +10,7 @@ export interface I18nPluginOptions {
 export function i18nJsonPlugin(options: I18nPluginOptions = {}): Plugin {
     const inputPath = options.inputPath || 'src/i18n';
     const outputDir = options.outputDir || 'public/locales';
+    const inputDir = inputPath.endsWith('.json') || inputPath.endsWith('.jsonc') ? path.dirname(inputPath) : inputPath;
     let singleFile : boolean = false;
     // if its a file wrap it in an array:
     if (inputPath.endsWith('.jsonc') || inputPath.endsWith('.json')) {
@@ -26,6 +28,28 @@ export function i18nJsonPlugin(options: I18nPluginOptions = {}): Plugin {
             } else {
                 await processI18nFiles(inputPath, outputDir);
             }
+        },
+        async handleHotUpdate(ctx) {
+
+            const changedDir = path.dirname(ctx.file);
+            const changedFile = ctx.file;
+
+            // is inputDir in the file path:
+            if (singleFile && changedFile.endsWith(inputPath) || !singleFile && changedDir.endsWith(inputDir)) {
+                if (singleFile) {
+                    await processI18nFile(inputPath, outputDir);
+                } else {
+                    await processI18nFiles(inputPath, outputDir);
+                }
+            } else {
+                return;
+            }
+        },
+        configureServer(server) {
+            // the path of outputDir use the config from vite.config.ts
+            const theAbsPath = path.resolve(server.config.root, outputDir);
+            // Replace with your actual output directory
+            server.watcher.unwatch(`${theAbsPath}/**`);
         }
     };
 }
